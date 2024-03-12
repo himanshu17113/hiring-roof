7ffeb19fcb9017d4e2fc697701d65af6463e0bff
Debashish <devmanna2002@gmail.com>
2024-03-12 18:39:03 +0530

    copyofmaincommit Co-authored-by: himanshu17113 <himanshu17113@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,10 @@ import 'util/theme.dart';
 class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
   }
 }
+//changedincopyofmain will push into this main/////
 
 void main() async {
   HttpOverrides.global = MyHttpOverrides();
